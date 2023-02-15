2fa51dccbf46a32853b7cc24b4da0c2dcdc00afb
Reed Martin <reedhmartin@gmail.com>
2023-02-15 18:18:30 -0500

    Added isNaN  to password prompt. Added confirms for character types. Added if statments to concat chara cters to allowable characters and ensure at least 1 is pushed into selected characters.

--- script.js
+++ script.js
@@ -11,8 +11,37 @@ function generatePassword() {
   var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   var passwordLength = prompt("Enter Desired Password Length. Must Be Between 8 and 128 Characters.");
-    while ((passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    while (isNan(passwordLength) || passwordLength < 8 || passwordLength > 128) {
     passwordLength = prompt("Invalid Length Entry. Length Must Be Between 8 and 128 Characters Long. Enter Desired Length of New Password.");
+  }
+
+  var useSpecialChars = confirm("Do you want to include special characters?");
+  var useUppercase = confirm("Do you want to include uppercase letters?");
+  var useLowercase = confirm("Do you want to include lowercase letters?");
+  var useNumbers = confirm("Do you want to include numbers?");
+
+  /** Array of chars that we can randomly pick from */
+  var allowableChars = [];
+    /** Array of chars that we have picked */
+  var selectedChars = [];
+
+  if (useSpecialChars == true) {
+    /** Add specialChars to selection */
+    allowableChars = allowableChars.concat(allowableChars, specialChars);
+    /** Push a special char into selectedChars array -- ensures at least 1 special char */
+    selectedChars.push(specialChars[Math.floor(Math.random() * specialChars.length)]);
+  } 
+  if (useUppercase == true) {
+    allowableChars = allowableChars.concat(allowableChars, uppercase);
+    selectedChars.push(uppercase[Math.floor(Math.random() * uppercase.length)]);
+  } 
+  if (useLowercase == true) {
+    allowableChars = allowableChars.concat(allowableChars, lowercase);
+    selectedChars.push(lowercase[Math.floor(Math.random() * lowercase.length)]);
+  } 
+  if (useNumbers == true) {
+    allowableChars = allowableChars.concat(allowableChars, numbers);
+    selectedChars.push(numbers[Math.floor(Math.random() * numbers.length)]);
   }
 
 }
